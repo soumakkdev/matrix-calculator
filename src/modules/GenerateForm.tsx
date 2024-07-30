66b2af57be8db5de523a8b6fba2ad55ec31a956b
soumakkdev <soumakkdutta@gmail.com>
2024-07-30 22:26:30 +0530

    error handling

--- src/modules/GenerateForm.tsx
+++ src/modules/GenerateForm.tsx
@@ -1,16 +1,22 @@
 'use client'
 import { Button } from '@/components/button'
 import { Input } from '@/components/Input'
-import React from 'react'
+import React, { useState } from 'react'
 import { useMatrix } from './MatrixContext'
 
 export default function GenerateForm() {
 	const { rows, setRows, cols, setCols, generateMatrixes } = useMatrix()
+	const [error, setError] = useState('')
 	return (
 		<form
 			onSubmit={(e) => {
 				e.preventDefault()
-				generateMatrixes()
+				if (rows > 0 && cols > 0) {
+					generateMatrixes()
+					setError('')
+				} else {
+					setError('Please provide a valid number')
+				}
 			}}
 			className="flex items-center space-x-4"
 		>
@@ -19,6 +25,8 @@ export default function GenerateForm() {
 			<label className="text-sm">Columns</label>
 			<Input type="number" min={1} value={cols} onChange={(e) => setCols(parseInt(e.target.value))} />
 			<Button>Generate</Button>
+
+			{error ? <p className="text-red-500 text-sm italic">{error}</p> : null}
 		</form>
 	)
 }
